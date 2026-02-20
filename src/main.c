84e4dc49905d20bcf3addf6140d5da333dc361d4
Tyrell Curry <tyrellcurryio@gmail.com>
2026-02-19 22:42:18 -0800

    Tests updated

--- src/main.c
+++ src/main.c
@@ -7,82 +7,83 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-int main()
-{
-	// Disable output buffering
-	setbuf(stdout, NULL);
-	setbuf(stderr, NULL);
+int main() {
+  // Disable output buffering
+  setbuf(stdout, NULL);
+  setbuf(stderr, NULL);
 
-	int server_fd;
-	socklen_t client_addr_len;
-	struct sockaddr_in client_addr;
+  int server_fd;
+  socklen_t client_addr_len;
+  struct sockaddr_in client_addr;
 
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) {
-		printf("Socket creation failed: %s...\n", strerror(errno));
-		return 1;
-	}
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd == -1) {
+    printf("Socket creation failed: %s...\n", strerror(errno));
+    return 1;
+  }
 
-	// Reuse port
-	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-		printf("setsockopt failed: %s\n", strerror(errno));
-		return 1;
-	}
+  // Reuse port
+  int reuse = 1;
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) <
+      0) {
+    printf("setsockopt failed: %s\n", strerror(errno));
+    return 1;
+  }
 
-	struct sockaddr_in serv_addr = {
-	    .sin_family = AF_INET,
-	    .sin_port = htons(4221),
-	    .sin_addr = {htonl(INADDR_ANY)},
-	};
+  struct sockaddr_in serv_addr = {
+      .sin_family = AF_INET,
+      .sin_port = htons(4221),
+      .sin_addr = {htonl(INADDR_ANY)},
+  };
 
-	if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0) {
-		printf("Bind failed: %s \n", strerror(errno));
-		return 1;
-	}
+  if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0) {
+    printf("Bind failed: %s \n", strerror(errno));
+    return 1;
+  }
 
-	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0) {
-		printf("Listen failed: %s \n", strerror(errno));
-		return 1;
-	}
+  int connection_backlog = 5;
+  if (listen(server_fd, connection_backlog) != 0) {
+    printf("Listen failed: %s \n", strerror(errno));
+    return 1;
+  }
 
-	printf("Waiting for a client to connect...\n");
-	client_addr_len = sizeof(client_addr);
+  printf("Waiting for a client to connect...\n");
+  client_addr_len = sizeof(client_addr);
 
-	int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-	printf("Client connected\n");
+  int client_fd =
+      accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+  printf("Client connected\n");
 
-	char buffer[4096];
+  char buffer[4096];
 
-	ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
-	if (bytes_read < 0) {
-		printf("Read failed: %s\n", strerror(errno));
-		close(client_fd);
-		close(server_fd);
-		return 1;
-	}
+  ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
+  if (bytes_read < 0) {
+    printf("Read failed: %s\n", strerror(errno));
+    close(client_fd);
+    close(server_fd);
+    return 1;
+  }
 
-	// Null-terminate the buffer so we can use string functions
-	buffer[bytes_read] = '\0';
+  // Null-terminate the buffer so we can use string functions
+  buffer[bytes_read] = '\0';
 
-	// Now parse the HTTP request
-	printf("Received request:\n%s\n", buffer);
+  // Now parse the HTTP request
+  printf("Received request:\n%s\n", buffer);
 
-	// Extract the request line (first line)
-	char method[16], path[256], version[16];
-	if (sscanf(buffer, "%15s %255s %15s", method, path, version) == 3) {
-		printf("Method: %s, Path: %s, Version: %s\n", method, path, version);
-	} else {
-		printf("Failed to parse request line\n");
-	}
+  // Extract the request line (first line)
+  char method[16], path[256], version[16];
+  if (sscanf(buffer, "%15s %255s %15s", method, path, version) == 3) {
+    printf("Method: %s, Path: %s, Version: %s\n", method, path, version);
+  } else {
+    printf("Failed to parse request line\n");
+  }
 
-	const char *http_response = "HTTP/1.1 200 OK\r\n\r\n";
+  const char *http_response = "HTTP/1.1 200 OK\r\n\r\n";
 
-	send(client_fd, http_response, strlen(http_response), 0);
+  send(client_fd, http_response, strlen(http_response), 0);
 
-	close(server_fd);
-	close(client_fd);
+  close(server_fd);
+  close(client_fd);
 
-	return 0;
+  return 0;
 }
