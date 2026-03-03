4f90cdfd2814fbdaf9f4112140f4136466200078
Tyrell Curry <tyrellcurryio@gmail.com>
2026-03-02 21:57:10 -0800

    Create socket file

---
+++ src/socket.c
@@ -0,0 +1,28 @@
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+// temp name to main for development/testing
+int main() {
+  int status;
+  struct addrinfo hints;
+  struct addrinfo *servinfo;
+  memset(&hints, 0, sizeof hints); // make sure the struct is empty
+  hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
+  hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+  hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+
+  if ((status = getaddrinfo(NULL, "3490", &hints, &servinfo)) != 0) {
+    fprintf(stderr, "gai error: %s\n", gai_strerror(status));
+    exit(1);
+  }
+
+  printf("Status: %d\n", status);
+  printf("socket file is running \n");
+
+  freeaddrinfo(servinfo);
+  return 1;
+}
