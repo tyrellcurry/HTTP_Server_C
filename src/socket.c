15011a82e0923d39a98ed17b82f001d32380deb1
Tyrell Curry <tyrellcurryio@gmail.com>
2026-03-04 21:27:12 -0800

    Refactor function

--- src/socket.c
+++ src/socket.c
@@ -1,28 +1,32 @@
 #include <netdb.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 
+struct AddrResult {
+  struct addrinfo *servinfo;
+  int error_code;
+};
+
+struct AddrResult generateAddrInfo() {
+  struct AddrResult result = {0};
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  result.error_code = getaddrinfo(NULL, "3490", &hints, &result.servinfo);
+  return result;
+}
+
 // temp name to main for development/testing
 int main() {
-  int status;
-  struct addrinfo hints;
-  struct addrinfo *servinfo;
-  memset(&hints, 0, sizeof hints); // make sure the struct is empty
-  hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
-  hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
-  hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
-
-  if ((status = getaddrinfo(NULL, "3490", &hints, &servinfo)) != 0) {
-    fprintf(stderr, "gai error: %s\n", gai_strerror(status));
-    exit(1);
+  struct AddrResult result = generateAddrInfo();
+  if (result.error_code != 0) {
+    fprintf(stderr, "Error: %s\n", gai_strerror(result.error_code));
+  } else {
+    freeaddrinfo(result.servinfo);
   }
-
-  printf("Status: %d\n", status);
-  printf("socket file is running \n");
-
-  freeaddrinfo(servinfo);
-  return 1;
 }
